e0a041d9fb6a8b06db8411975beb469d34fefd6e
Amey <ameypte@gmail.com>
2024-09-28 23:45:39 +0530

    admin side bar basic UI added

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Navbar from '@/components/admin/navbar'
+"use client";
+import {useState} from 'react'
+import SideBar from '@/components/admin/SideBar'
+import Dashboard from '@/components/admin/Dashboard'
+import GenerateQuestion from '@/components/admin/GenerateQuestion'
 
 export default function page() {
+  const [tab, setTab] = useState('dashboard')
   return (
     <div>
-        <Navbar />
+        <SideBar tab={tab} setTab={setTab}>
+            {tab === 'dashboard' && <Dashboard />}
+            {tab === 'generate' && <GenerateQuestion />}
+        </SideBar>
     </div>
   )
 }
