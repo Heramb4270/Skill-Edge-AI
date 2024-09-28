793af312c488f64f94d22f8e3d2886c9b9fecf02
Amey <ameypte@gmail.com>
2024-09-29 00:05:07 +0530

    added stident list tab in admin

--- frontend/app/admin/page.js
+++ frontend/app/admin/page.js
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import SideBar from '@/components/admin/SideBar'
 import Dashboard from '@/components/admin/Dashboard'
 import GenerateQuestion from '@/components/admin/GenerateQuestion'
+import StudentList from '@/components/admin/StudentList'
 
 export default function page() {
   const [tab, setTab] = useState('dashboard')
@@ -11,6 +12,7 @@ export default function page() {
         <SideBar tab={tab} setTab={setTab}>
             {tab === 'dashboard' && <Dashboard />}
             {tab === 'generate' && <GenerateQuestion />}
+            {tab === 'students' && <StudentList />}
         </SideBar>
     </div>
   )
