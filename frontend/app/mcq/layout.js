4d453a99cc6887255ba0a488ae03774425de90bd
Heramb Bhoodhar <heramb.bhoodhar@gmail.com>
2024-09-29 01:31:32 +0530

    Homepage Added

---
+++ frontend/app/mcq/layout.js
@@ -0,0 +1,29 @@
+// "use  client"
+// import { MdPhishing } from "react-icons/md";
+// import { PiPasswordFill } from "react-icons/pi";
+// import { AiOutlineFileProtect } from "react-icons/ai";
+// import { MdPeopleAlt } from "react-icons/md";
+// import { GoReport } from "react-icons/go";
+// import { FaNetworkWired } from "react-icons/fa6";
+// import SideBar from "@/components/SideBar/SideBar";
+// import SideBar from "@/components/admin/SideBar";
+// import { useState } from "react";
+// import Ecommerce from "@/components/Ecommerce";
+
+export default function RootLayout({ children }) {
+  return (
+    <>
+      <div class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        {/* <NavBar /> */}
+      </div>
+
+      {/* <SideBar /> */}
+
+      <div class="p-4 sm:ml-64 bg-gray-100 dark:bg-gray-800">
+        <div class="p-4 mt-20 bg-white rounded-lg dark:bg-gray-900">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,90 @@
-import Image from "next/image";
-import Navbar from "../components/user/navbar";
+"use client";
+import Card from "@/components/Card";
+import Footer from "@/components/Footer";
+import GetCertificateButton from "@/components/GetCertificateButton";
+import IntoBanner from "@/components/IntroBanner";
+// import NavBar from "@/components/user/Navbar";
+import Navbar from "@/components/user/navbar";
+import ProgressBar from "@/components/ProgressBar";
+import Title from "@/components/Title";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [isLogged, setIsLogged] = useState(false);
+  const [username, setUsername] = useState(null);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    if (localStorage.getItem("name")) {
+      setUsername(() => localStorage.getItem("name"));
+      setIsLogged(() => true);
+    } else {
+      setUsername(() => null);
+      setIsLogged(() => false);
+    }
+
+    getProgress();
+  }, []);
+
+  const getProgress = async () => {
+    const user_id = localStorage.getItem("user_id");
+    const totalModules = 6;
+
+    const response = await fetch("/api/progress", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user_id, totalModules }),
+    });
+
+    const data = await response.json();
+    setProgress(() => data.progress);
+  };
+
   return (
-   <div>
+    <div className="bg-gray-50 dark:bg-gray-900">
       <Navbar />
-   </div>
+      <IntoBanner username={username} />
+      {/* <hr /> */}
+      <div className="max-w-screen-xl mx-auto">
+     
+        <Title />
+        <div className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-3">
+          <Card
+            img={"/img2.jpg"}
+            title={"Operating Systems Concepts (OS)"}
+            link={isLogged ? "/training/phishing" : "/login"}
+          />
+          <Card
+            img={"/img4.jpg"}
+            title={"Database Management Systems"}
+            link={isLogged ? "/training/pass-security" : "/login"}
+          />
+          <Card
+            img={"/dataprotection.jpg"}
+            title={"Data Structures and Algorithms"}
+            link={isLogged ? "/training/data-protection" : "/login"}
+          />
+          <Card
+            img={"/img6.jpg"}
+            title={"Computer Networks Basics (CN)"}
+            link={isLogged ? "/training/social-engg" : "/login"}
+          />
+          <Card
+            img={"/incidence.jpg"}
+            title={"Artificial Intelligence and Machine Learning"}
+            link={isLogged ? "/training/incidence-res" : "/login"}
+          />
+          <Card
+            img={"/remote.jpg"}
+            title={"Web Development and Frameworks"}
+            link={isLogged ? "/training/remote-work" : "/login"}
+          />
+        </div>
+      </div>
+
+      <Footer />
+    </div>
   );
 }
