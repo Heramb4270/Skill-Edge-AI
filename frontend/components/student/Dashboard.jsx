0227da10f714e402ec9e29716a217f6116cd4182
DurevshChaudhari <durveshchaudhari11@gmail.com>
2024-10-10 11:41:11 +0530

    Student side partially DOne

---
+++ frontend/components/student/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div className="bg-white dark:bg-gray-800 text-black dark:text-white p-4 rounded-lg">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
+  )
+}
--- frontend/components/student/SideBar.js
+++ frontend/components/student/SideBar.js
@@ -81,18 +81,11 @@ export default function SideBar({ children, setTab, tab }) {
                             <a href="#" className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${tab === 'generate' ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
                                 onClick={() => setTab('generate')}>
                                 <IoSettings className={`w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white ${tab === 'generate' ? 'text-gray-900 dark:text-white' : ''}`} />
-                                <span className="flex-1 ms-3 whitespace-nowrap">Generate Questions</span>
+                                <span className="flex-1 ms-3 whitespace-nowrap">Practise Test</span>
                                 {/* <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span> */}
                             </a>
                         </li>
-                        <li>
-                            <a href="#" className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${tab === 'students' ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
-                                onClick={() => setTab('students')}>
-                                <BsFillPeopleFill className={`w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white ${tab === 'students' ? 'text-gray-900 dark:text-white' : ''}`} />
-                                <span className="flex-1 ms-3 whitespace-nowrap">Students List</span>
-                                {/* <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span> */}
-                            </a>
-                        </li>
+                       
                         <li>
                             <a href="#" className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${tab === 'resource' ? 'bg-gray-200 dark:bg-gray-700' : ''}`}
                                 onClick={() => setTab('resource')}>
